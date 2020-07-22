586b37ed2bdf3429a81b2fe6d65c261f5c5f355e
Babich M <astilba22@gmail.com>
2020-07-23 01:04:11 +0300

    add ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    ADD_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main > .price_color')
+    BASKET_PRICE = (By.CSS_SELECTOR, '#messages > div:nth-child(3) strong')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main > h1')
+    BASKET_PRODUCT_NAME = (By.CSS_SELECTOR, '#messages > div:nth-child(1) strong')
---
+++ pages/product_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product(self):
+        button = self.browser.find_element(*ProductPageLocators.ADD_BUTTON)
+        button.click()
+
+    def should_be_add_button(self):
+        assert self.is_element_present(*ProductPageLocators.ADD_BUTTON), 'Add button is not presented'
+
+    def should_be_correct_basket_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_price = self.browser.find_element(*ProductPageLocators.BASKET_PRICE).text
+        assert product_price == basket_price, 'Incorrect basket price in messages'
+
+    def should_be_correct_product_name(self):
+        real_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        basket_product_name = self.browser.find_element(*ProductPageLocators.BASKET_PRODUCT_NAME).text
+        assert real_name == basket_product_name, 'Incorrect product name in messages'
